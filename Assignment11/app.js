6f1c4997cfd77a4d10ab91cf8845d4927e1087c9
Ali Shakeel <148653842+AliShakeel62@users.noreply.github.com>
2023-11-30 01:01:52 +0500

    Add files via upload

---
+++ Assignment11/app.js
@@ -0,0 +1,8 @@
+
+var first = prompt("Enter Student Math Marks");
+var second =prompt("Enter Student Math Marks");
+var theird =prompt("Enter Student Math Marks");
+var fourth =prompt("Enter Student Math Marks");
+var fifth =prompt("Enter Student Math Marks");
+alert ( "85%" );
+console.log ( "85%  is total persentage" )
